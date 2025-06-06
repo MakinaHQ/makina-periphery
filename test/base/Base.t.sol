05c15343fd5c0f6dd6e42c3f3c7361f4c153424e
clempetit <clementpetit.sg@gmail.com>
2025-06-06 15:22:09 +0200

    chore: add fork test setup + FlashloanAggregator tests

--- test/base/Base.t.sol
+++ test/base/Base.t.sol
@@ -6,28 +6,24 @@ import {Test} from "forge-std/Test.sol";
 import "@makina-core-test/base/Base.sol" as Core_base;
 
 import {AccessManagerUpgradeable} from "@openzeppelin/contracts-upgradeable/access/manager/AccessManagerUpgradeable.sol";
-import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 import {MockWormhole} from "@makina-core-test/mocks/MockWormhole.sol";
-import {Caliber} from "@makina-core/caliber/Caliber.sol";
 import {ChainRegistry} from "@makina-core/registries/ChainRegistry.sol";
 import {HubCoreRegistry} from "@makina-core/registries/HubCoreRegistry.sol";
-import {Machine} from "@makina-core/machine/Machine.sol";
 import {HubCoreFactory} from "@makina-core/factories/HubCoreFactory.sol";
 import {OracleRegistry} from "@makina-core/registries/OracleRegistry.sol";
-import {PreDepositVault} from "@makina-core/pre-deposit/PreDepositVault.sol";
 import {SwapModule} from "@makina-core/swap/SwapModule.sol";
 import {TokenRegistry} from "@makina-core/registries/TokenRegistry.sol";
 
 import {Constants} from "../utils/Constants.sol";
+import {CoreHelpers} from "../utils/CoreHelpers.sol";
 import {FlashloanAggregator} from "../../src/flashloans/FlashloanAggregator.sol";
 import {HubPeripheryRegistry} from "../../src/registries/HubPeripheryRegistry.sol";
 import {HubPeripheryFactory} from "../../src/factories/HubPeripheryFactory.sol";
 
 import {Base} from "./Base.sol";
 
-abstract contract Base_Test is Base, Test, Constants {
+abstract contract Base_Test is Base, Test, Constants, CoreHelpers {
     address public deployer;
 
     uint256 public hubChainId;
@@ -75,7 +71,7 @@ abstract contract Base_Hub_Test is Base_Test {
         Base_Test.setUp();
         hubChainId = block.chainid;
 
-        wormhole = _deployWormhole();
+        wormhole = _deployWormhole(WORMHOLE_HUB_CHAIN_ID, hubChainId);
 
         balancerV2Pool = makeAddr("BalancerV2Pool");
         balancerV3Pool = makeAddr("BalancerV3Pool");
@@ -85,7 +81,7 @@ abstract contract Base_Hub_Test is Base_Test {
         dai = makeAddr("DAI");
 
         // Hub Core
-        Core_base.Base.HubCore memory hubCore = _deployHubCore();
+        Core_base.Base.HubCore memory hubCore = _deployHubCore(deployer, dao, address(wormhole));
         accessManager = hubCore.accessManager;
         oracleRegistry = hubCore.oracleRegistry;
         swapModule = hubCore.swapModule;
@@ -99,13 +95,13 @@ abstract contract Base_Hub_Test is Base_Test {
             address(accessManager),
             address(hubCore.hubCoreFactory),
             dao,
-            FlashloanProviders({
-                _balancerV2Pool: balancerV2Pool,
-                _balancerV3Pool: balancerV3Pool,
-                _morphoPool: morphoPool,
-                _dssFlash: dssFlash,
-                _aaveV3AddressProvider: aaveV3AddressProvider,
-                _dai: dai
+            FlashLoanAggregatorInitParams({
+                balancerV2Pool: balancerV2Pool,
+                balancerV3Pool: balancerV3Pool,
+                morphoPool: morphoPool,
+                dssFlash: dssFlash,
+                aaveV3AddressProvider: aaveV3AddressProvider,
+                dai: dai
             })
         );
         flashloanAggregator = hubPeriphery.flashloanAggregator;
@@ -114,117 +110,6 @@ abstract contract Base_Hub_Test is Base_Test {
 
         registerFlashloanAggregator(address(hubCore.hubCoreRegistry), address(flashloanAggregator));
         registerHubPeripheryFactory(address(hubPeripheryRegistry), address(hubPeripheryFactory));
-        _setupAccessManager();
-    }
-
-    function _deployWormhole() internal returns (MockWormhole _wormhole) {
-        _wormhole = new MockWormhole(WORMHOLE_HUB_CHAIN_ID, hubChainId);
-    }
-
-    function _deployHubCore() internal returns (Core_base.Base.HubCore memory deployment) {
-        address accessManagerImplemAddr = address(new AccessManagerUpgradeable());
-        deployment.accessManager = AccessManagerUpgradeable(
-            address(
-                new TransparentUpgradeableProxy(
-                    accessManagerImplemAddr, dao, abi.encodeCall(AccessManagerUpgradeable.initialize, (deployer))
-                )
-            )
-        );
-
-        address oracleRegistryImplemAddr = address(new OracleRegistry());
-        deployment.oracleRegistry = OracleRegistry(
-            address(
-                new TransparentUpgradeableProxy(
-                    oracleRegistryImplemAddr,
-                    dao,
-                    abi.encodeCall(OracleRegistry.initialize, (address(deployment.accessManager)))
-                )
-            )
-        );
-
-        address tokenRegistryImplemAddr = address(new TokenRegistry());
-        deployment.tokenRegistry = TokenRegistry(
-            address(
-                new TransparentUpgradeableProxy(
-                    tokenRegistryImplemAddr,
-                    dao,
-                    abi.encodeCall(TokenRegistry.initialize, (address(deployment.accessManager)))
-                )
-            )
-        );
-
-        address chainRegistryImplemAddr = address(new ChainRegistry());
-        deployment.chainRegistry = ChainRegistry(
-            address(
-                new TransparentUpgradeableProxy(
-                    chainRegistryImplemAddr,
-                    dao,
-                    abi.encodeCall(ChainRegistry.initialize, (address(deployment.accessManager)))
-                )
-            )
-        );
-
-        address hubCoreRegistryImplemAddr = address(new HubCoreRegistry());
-        deployment.hubCoreRegistry = HubCoreRegistry(
-            address(
-                new TransparentUpgradeableProxy(
-                    hubCoreRegistryImplemAddr,
-                    dao,
-                    abi.encodeCall(
-                        HubCoreRegistry.initialize,
-                        (
-                            address(deployment.oracleRegistry),
-                            address(deployment.tokenRegistry),
-                            address(deployment.chainRegistry),
-                            address(deployment.accessManager)
-                        )
-                    )
-                )
-            )
-        );
-
-        address swapModuleImplemAddr = address(new SwapModule(address(deployment.hubCoreRegistry)));
-        deployment.swapModule = SwapModule(
-            address(
-                new TransparentUpgradeableProxy(
-                    swapModuleImplemAddr,
-                    dao,
-                    abi.encodeCall(SwapModule.initialize, (address(deployment.accessManager)))
-                )
-            )
-        );
-
-        address caliberImplemAddr = address(new Caliber(address(deployment.hubCoreRegistry), address(0)));
-        deployment.caliberBeacon = new UpgradeableBeacon(caliberImplemAddr, dao);
-
-        address machineImplemAddr = address(new Machine(address(deployment.hubCoreRegistry), address(wormhole)));
-        deployment.machineBeacon = new UpgradeableBeacon(machineImplemAddr, dao);
-
-        address preDepositVaultImplemAddr = address(new PreDepositVault(address(deployment.hubCoreRegistry)));
-        deployment.preDepositVaultBeacon = new UpgradeableBeacon(preDepositVaultImplemAddr, dao);
-
-        address hubCoreFactoryImplemAddr = address(new HubCoreFactory(address(deployment.hubCoreRegistry)));
-        deployment.hubCoreFactory = HubCoreFactory(
-            address(
-                new TransparentUpgradeableProxy(
-                    hubCoreFactoryImplemAddr,
-                    dao,
-                    abi.encodeCall(HubCoreFactory.initialize, (address(deployment.accessManager)))
-                )
-            )
-        );
-
-        deployment.hubCoreRegistry.setSwapModule(address(deployment.swapModule));
-        deployment.hubCoreRegistry.setTokenRegistry(address(deployment.tokenRegistry));
-        deployment.hubCoreRegistry.setChainRegistry(address(deployment.chainRegistry));
-        deployment.hubCoreRegistry.setCoreFactory(address(deployment.hubCoreFactory));
-        deployment.hubCoreRegistry.setCaliberBeacon(address(deployment.caliberBeacon));
-        deployment.hubCoreRegistry.setMachineBeacon(address(deployment.machineBeacon));
-        deployment.hubCoreRegistry.setPreDepositVaultBeacon(address(deployment.preDepositVaultBeacon));
-    }
-
-    function _setupAccessManager() internal {
-        accessManager.grantRole(accessManager.ADMIN_ROLE(), dao, 0);
-        accessManager.revokeRole(accessManager.ADMIN_ROLE(), address(this));
+        _setupAccessManager(accessManager, dao);
     }
 }
